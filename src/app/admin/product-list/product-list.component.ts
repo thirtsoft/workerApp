f3b58486df97fccdada24491cce3803c32e402a0
thirdiallo <thirdiallo@gmail.com>
2022-06-30 01:26:46 +0200

    crud addresse

--- src/app/admin/product-list/product-list.component.ts
+++ src/app/admin/product-list/product-list.component.ts
@@ -9,6 +9,10 @@ import {
 import { CommonServiceService } from '../../common-service.service';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import * as $ from 'jquery';
+import { Address } from 'src/app/models/address';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AddressService } from 'src/app/services/address.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-product-list',
@@ -17,18 +21,36 @@ import * as $ from 'jquery';
 })
 export class ProductListComponent implements OnInit {
   products = [];
+  addressList: any = [];
   modalRef: BsModalRef;
   errorMessage: string;
   name;
   id;
   key;
+  formDataAddress = new Address();
+  editForm: FormGroup;
+  p : number=1;
+  searchText;
+
   constructor(
     private commonService: CommonServiceService,
+    private addService: AddressService,
+    private toastr: ToastrService,
+    private fb: FormBuilder,
     private modalService: BsModalService
   ) {}
 
   ngOnInit(): void {
     this.getProducts();
+    this.getAddressLists();
+    this.editForm = this.fb.group({
+      id: [''],
+      name: [''],
+      code: [''],
+      country: [''],
+      dataCreate: new Date(),
+      dataUpdate: new Date(),
+    } );
   }
 
   getProducts() {
@@ -43,12 +65,68 @@ export class ProductListComponent implements OnInit {
     );
   }
 
+  getAddressLists() {
+    this.addService.getAllAddresss().subscribe(
+      (data: any[]) => {
+        this.addressList = data;
+        $(function () {
+          $('table').DataTable();
+        });
+      },
+      (error) => (this.errorMessage = <any>error)
+    );
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, {
+      class: 'modal-lg modal-dialog-centered',
+    });
+  }
+
+  save() {
+    console.log(this.formDataAddress);
+    this.addService.addAddress(this.formDataAddress)
+    .subscribe(data => {
+      this.modalRef.hide();
+      this.getAddressLists();
+    });
+    this.modalRef.hide();
+    this.formDataAddress = null;
+  }
+
   editModal(template: TemplateRef<any>, category) {
     this.id = category.id;
     this.name = category.name;
     this.modalRef = this.modalService.show(template, {
       class: 'modal-sm modal-dialog-centered',
     });
+  }
+
+  editAddressModal(template: TemplateRef<any>, add: Address) {
+    this.modalRef = this.modalService.show(template, {
+      class: 'modal-lg modal-dialog-centered',
+    });
+    this.editForm.patchValue( {
+      id: add.id,
+      code: add.code,
+      name: add.name,
+      country: add.country,
+      dataCreate: add.dataCreate,
+      dataUpdate: add.dataUpdate
+    });
+  }
+
+  updateAddress() {
+    this.addService.updateAddress(this.editForm.value.id, this.editForm.value)
+      .subscribe((data) => {
+        this.toastr.success('avec succès','Information modifiée', {
+          timeOut: 1500,
+          positionClass: 'toast-top-right',
+        });
+        this.modalRef.hide();
+        this.getAddressLists();
+      });
+    this.modalRef.hide();
   }
 
   update() {
@@ -60,23 +138,25 @@ export class ProductListComponent implements OnInit {
     this.modalRef.hide();
   }
 
-  openModal(template: TemplateRef<any>) {
+  deleteAddressModal(template: TemplateRef<any>, add) {
+    this.id = add.id;
     this.modalRef = this.modalService.show(template, {
       class: 'modal-sm modal-dialog-centered',
     });
   }
 
-  deleteModal(template: TemplateRef<any>, product) {
-    this.id = product.id;
-    this.modalRef = this.modalService.show(template, {
-      class: 'modal-sm modal-dialog-centered',
+  deleteAddress() {
+    this.addressList = this.addressList.filter((a) => a.id !== this.id);
+    this.addService.deleteAddress(this.id).subscribe((data) => {
+      this.toastr.error('avec succès','Address Supprimé', {
+        timeOut: 1500,
+        positionClass: 'toast-top-right',
+      });
+      this.modalRef.hide();
+      this.getAddressLists();
     });
   }
-
-  save() {
-    this.modalRef.hide();
-  }
-
+ 
   deleteProduct() {
     this.products = this.products.filter((a) => a.id !== this.id);
     this.commonService.deleteProduct(this.id).subscribe((data: any[]) => {
@@ -88,4 +168,25 @@ export class ProductListComponent implements OnInit {
   decline() {
     this.modalRef.hide();
   }
+
+  btnColor() {
+    document.getElementById('btn-yes').style.backgroundColor = '#00d0f1';
+    document.getElementById('btn-yes').style.border = '1px solid #00d0f1';
+    document.getElementById('btn-yes').style.color = '#fff';
+
+    document.getElementById('btn-no').style.backgroundColor = '#fff';
+    document.getElementById('btn-no').style.border = '1px solid #fff';
+    document.getElementById('btn-no').style.color = '#000';
+  }
+
+  btnColorNo() {
+    document.getElementById('btn-no').style.backgroundColor = '#00d0f1';
+    document.getElementById('btn-no').style.border = '1px solid #00d0f1';
+    document.getElementById('btn-no').style.color = '#fff';
+
+    document.getElementById('btn-yes').style.backgroundColor = '#fff';
+    document.getElementById('btn-yes').style.border = '1px solid #fff';
+    document.getElementById('btn-yes').style.color = '#000';
+  }
+
 }
--- src/app/admin/product-list/product-list.module.ts
+++ src/app/admin/product-list/product-list.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductListRoutingModule } from './product-list-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ ProductListComponent ],
   imports: [
     CommonModule,
-    ProductListRoutingModule
+    ProductListRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductListModule { }
