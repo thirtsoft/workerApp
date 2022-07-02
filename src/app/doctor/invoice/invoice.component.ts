c8533066b6164944f764dd5ac18b7daa3db97bd7
thirdiallo <thirdiallo@gmail.com>
2022-07-02 03:30:10 +0200

    handler jeton list of customer

--- src/app/doctor/invoice/invoice.component.ts
+++ src/app/doctor/invoice/invoice.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenStorageService } from 'src/app/services/auth/security/token-storage.service';
+import { JetonService } from 'src/app/services/jeton.service';
 
 import { CommonServiceService } from './../../common-service.service';
 
@@ -10,16 +13,59 @@ import { CommonServiceService } from './../../common-service.service';
 export class InvoiceComponent implements OnInit {
 
   invoices : any = [];
-  constructor(public commonService:CommonServiceService) { }
+  jetonList: any = [];
+
+  info: any;
+  private roles: string[];
+
+  currentTime: number = 0;
+
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showUserBoard = false;
+  showVendeurBoard = false;
+
+  username: string;
+  email: String;
+  userId;
+  photo;
+  img: boolean;
+
+  currentUser;
+
+
+  constructor(public commonService:CommonServiceService,
+              private jet:JetonService,
+              private tokenService: TokenStorageService,
+              private router: Router) { }
 
   ngOnInit(): void {
   	this.getTransactions();
+    this.isLoggedIn = !!this.tokenService.getToken();
+    if (this.isLoggedIn) {
+      const user = this.tokenService.getUser();
+      this.roles = user.roles;
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showUserBoard = this.roles.includes('ROLE_USER');
+      this.username = user.username;
+      this.userId = user.id;
+      console.log(this.userId);
+    }
+    this.getJetonsList();
   }
 
   getTransactions() {
   	this.commonService.getTransactions()
   		.subscribe(res=>{
   			this.invoices = res;
+  		})
+  }
+
+  getJetonsList() {
+    console.log(this.userId);
+  	this.jet.getJetonsByCustomerIdByIdDesc(this.userId)
+  		.subscribe(res=>{
+  			this.jetonList = res;
   		})
   }
 
